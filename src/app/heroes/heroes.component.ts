5250a4cfc947a3edb2612e9a6c84b185315f5880
Ricky McConnell <mr.mcconnell12@yahoo.com>
2019-11-19 19:18:48 -0600

    added routing -- nav to dashboard, heroes list, and details for each

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+
 import { Hero } from "../hero";
 import { HeroService } from "../hero.service";
 
@@ -9,16 +10,11 @@ import { HeroService } from "../hero.service";
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
 
   constructor(private heroService: HeroService) {}
 
   ngOnInit() {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 
   getHeroes(): void {
